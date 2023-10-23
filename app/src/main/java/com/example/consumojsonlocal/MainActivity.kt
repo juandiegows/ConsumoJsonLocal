e086f2fd2b73b1f41c7dc3e284ca4471955bc897
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2023-10-23 18:56:01 -0500

    convertir texto a List

--- app/src/main/java/com/example/consumojsonlocal/MainActivity.kt
+++ app/src/main/java/com/example/consumojsonlocal/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.consumojsonlocal.databinding.ActivityMainBinding
 import com.example.consumojsonlocal.models.User
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
+import java.lang.reflect.Type
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        consumoRaw()
+        binding.txtTexto.text = consumoRaw().size.toString()
     }
 
     fun consumoRaw(): List<User> {
@@ -31,8 +34,11 @@ class MainActivity : AppCompatActivity() {
                 jsonString.append(line)
             }
             val json = jsonString.toString()
-            binding.txtTexto.text = json;
-            // Ahora 'json' contiene el contenido del archivo JSON.
+
+
+             val listType: Type? = object : TypeToken<List<User>>() {}.type
+             return  Gson().fromJson(json, listType)
+
         } catch (e: IOException) {
             // Manejar excepciones aquí
         }

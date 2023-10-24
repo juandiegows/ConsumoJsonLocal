7590203ba1530fbc305bda60664f180aa29c6b24
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2023-10-23 19:52:16 -0500

    leer archivos desde el almacenamiento del celular

--- app/src/main/java/com/example/consumojsonlocal/MainActivity.kt
+++ app/src/main/java/com/example/consumojsonlocal/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.consumojsonlocal
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.consumojsonlocal.adapter.UserAdapter
@@ -9,7 +14,10 @@ import com.example.consumojsonlocal.models.User
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.BufferedReader
+import java.io.File
+import java.io.FileInputStream
 import java.io.IOException
+import java.io.InputStream
 import java.io.InputStreamReader
 import java.lang.reflect.Type
 
@@ -25,10 +33,49 @@ binding.apply {
     btnraw.setOnClickListener {
         spinnner.adapter = UserAdapter(consumoRaw())
     }
+    btnAlmacenamiento.setOnClickListener {
+        consumoAlmacenamiento()
+    }
 }
 
     }
+    var openFileLauncher: ActivityResultLauncher<Intent>  = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            val uri: Uri? = data?.data
+            try {
+                val inputStream: InputStream? = contentResolver.openInputStream(uri!!)
+                val reader = BufferedReader(InputStreamReader(inputStream))
+                val jsonString = StringBuilder()
+                var line: String?
 
+                while (reader.readLine().also { line = it } != null) {
+                    jsonString.append(line)
+                }
+
+                val json = jsonString.toString()
+
+                val listType: Type? = object : TypeToken<List<User>>() {}.type
+                var list : List<User> = Gson().fromJson(json, listType)
+                binding.spinnner.adapter = UserAdapter(list)
+                // Ahora 'json' contiene el contenido del archivo JSON.
+            } catch (e: IOException) {
+                // Manejar excepciones aquí
+                e.printStackTrace()
+            }
+            // Uri contiene la ubicación del archivo seleccionado
+            // Puedes trabajar con el archivo a través de la Uri aquí
+            // Por ejemplo, puedes abrirlo en un visor o procesarlo en tu aplicación
+        }
+    }
+    fun consumoAlmacenamiento() {
+
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "*/*" /// Esto permite seleccionar cualquier tipo de archivo
+        openFileLauncher.launch(intent)
+    }
     fun consumoRaw(): List<User> {
         val res = resources
 
